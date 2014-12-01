4b73a2acbe6a5222ec3c043789cc25299033a6a7
Pramit Choudhary <pramitchoudhary@gmail.com>
2014-11-30 17:36:08 -0800

    installation step and a basic App

---
+++ sparkApps/src/main/scala/com/cloudera/sparkwordcount/SparkWordCount.scala
@@ -0,0 +1,27 @@
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+
+object SparkWordCount {
+  def main(args: Array[String]) {
+    val sc = new SparkContext(new SparkConf().setAppName("Spark Count"))
+    val threshold = args(1).toInt
+
+    // split each document into words
+    val tokenized = sc.textFile(args(0)).flatMap(_.split(" "))
+
+    // count the occurrence of each word
+    val wordCounts = tokenized.map((_, 1)).reduceByKey(_ + _)
+
+    // Print out the values
+    // wordCounts.collect.foreach(println)
+
+    // Save the output in a file
+    // wordCounts.saveAsTextFile("output.txt")
+
+    // filter out words with less than threshold occurrences
+    val filtered = wordCounts.filter(_._2 >= threshold)
+    filtered.collect.foreach(println)
+
+  }
+}
